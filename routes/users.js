442d6d4810e29949defa882a33a240aae270ab2a
juzlov <60822205+juzlov@users.noreply.github.com>
2020-04-08 21:50:44 +0300

    Revert "Developed local server with API and authentication"

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getUsers, getUserById, addUser } = require('../controllers/users');
-
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.post('/', addUser);
-
-module.exports = router;
