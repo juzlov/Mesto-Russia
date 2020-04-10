d0d2724ac813421918c5248166c17b00c03c7446
Ignat Anashin <firebat75@yandex.ru>
2020-04-10 20:20:04 +0300

    hardcode deleted

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5e8e0b7dbacbec378c0f4247',
-  };
-  next();
-});
-
 app.post('/signin', login);
 app.post('/signup', addUser);
 
