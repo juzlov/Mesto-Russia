3770947f513257caf72418bba4a118696250b3ca
Ignat Anashin <firebat75@yandex.ru>
2020-04-17 12:56:36 +0300

    implementing errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,12 +1,13 @@
 const Card = require('../models/card');
+const NotFoundError = require('../errors/NotFoundError');
+const Forbidden = require('../errors/Forbidden');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate('owner')
     .then((cards) => {
       if (!cards) {
-        res.status(404).send({ message: 'no cards were found' });
-        return;
+        throw new NotFoundError('no cards were found');
       }
       res.status(200).send({ data: cards });
     })
@@ -24,12 +25,10 @@ module.exports.removeCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'no cards with this id' });
-        return;
+        throw new NotFoundError('no cards with this id');
       }
       if (!card.owner.equals(req.user._id)) {
-        res.status(404).send({ message: 'you can delete only yours cards' });
-        return;
+        throw new Forbidden('you can delete only yours cards');
       }
       Card.findByIdAndDelete(req.params.cardId)
         .then((cards) => res.status(200).send({ data: cards }));
