16bf5170652ace10f93f9dd584796bd296e84559
Ignat Anashin <firebat75@yandex.ru>
2020-04-19 21:49:14 +0300

    validation url avatar added

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required(),
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().required().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/),
     email: Joi.string().required().email({ minDomainSegments: 2 }),
     password: Joi.string().required().min(6),
   }),
