f55bcb9e506ed879797e45923340dc30cc1a348c
Ignat Anashin <firebat75@yandex.ru>
2020-04-19 21:31:20 +0300

    linting errors solved

--- routes/cards.js
+++ routes/cards.js
@@ -6,6 +6,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
     link: Joi.string().required().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/),
   }),
 }), addCard);
